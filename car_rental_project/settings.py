fcc7f6e2082892e47a78c8e363d53a4dcde8d920
Abdillah <abdillahyoussouf15@gmail.com>
2026-02-14 00:19:43 +0100

    Connexion PostgreSQL

--- car_rental_project/settings.py
+++ car_rental_project/settings.py
@@ -65,11 +65,12 @@ WSGI_APPLICATION = 'car_rental_project.wsgi.application'
 
 # Database
 # Utilise SQLite en local, mais prêt pour une DB externe sur Render si besoin
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 # Internationalization
