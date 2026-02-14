c2159cc8b88c89fadc657cc7940ad22e114dd045
Abdillah <abdillahyoussouf15@gmail.com>
2026-02-14 01:45:44 +0100

    Ajout des images permanent

--- car_rental_project/settings.py
+++ car_rental_project/settings.py
@@ -5,7 +5,7 @@ Modified for Render deployment.
 
 from pathlib import Path
 import os
-import dj_database_url # Optionnel mais recommandé pour Render
+import dj_database_url 
 
 # Build paths
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,7 +80,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static & Media Configuration
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Dossier où Django va rassembler tous les fichiers statiques pour la prod
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
